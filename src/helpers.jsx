d2abbe85141515452280a50517f3ada1630ba107
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-06 13:57:11 +0530

    feat: Add helper functions file helpers.jsx

---
+++ src/helpers.jsx
@@ -0,0 +1,4 @@
+// Local storage functions
+export const fetchData = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+};
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,12 @@
+//helper functions
+import { fetchData } from "../helpers"
+
+// loader
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  return { userName }
+}
+
 const Dashboard = () => {
   return (
     <div>
