e079d505a80d2b3a5e6f2a5570d010fa35c1b462
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-06 17:05:25 +0530

    feat: Customize the error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,27 @@
+// rrd imports
+import { Link, useNavigate, useRouteError } from "react-router-dom"
+
+// library
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid"
+
 const Error = () => {
+  const error = useRouteError();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Error
+    <div className="error">
+      <h1>Houston, we have a problem!</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button className="btn btn--dark" onClick={() => navigate(-1)}>
+          <span>Go Back</span>
+          <ArrowUturnLeftIcon width={20}/>
+        </button>
+        <Link to="/" className="btn btn--primary">
+          <HomeIcon width={20}/>
+          <span>Go Home</span>
+        </Link>
+      </div>
     </div>
   )
 }
