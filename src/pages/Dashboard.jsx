58baf89b8d09cd7385cbd01607ab6b167e9dda7a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-06 16:46:46 +0530

    feat: Make intro form functional

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,10 +5,25 @@ import Intro from "../components/Intro";
 import { useLoaderData } from "react-router-dom";
 import { fetchData } from "../helpers"
 
+// library
+import { toast } from "react-toastify";
+
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
   return { userName }
+}
+
+// actions
+export async function dashboardAction({request}) {
+  const data = await request.formData()
+  const formData = Object.fromEntries(data)
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName))
+    return toast.success(`Welcome, ${formData.userName} 👋`)
+  } catch(e) {
+    throw new Error("Creating account was not successful.")
+  }
 }
 
 const Dashboard = () => {
