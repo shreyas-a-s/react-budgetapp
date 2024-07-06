227ed67c906be863970e41ca68df2d01b2ee48b2
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-06 13:45:33 +0530

    feat: Use react-router-dom in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hi</h1>
+  },
+]);
+
 function App() {
 
   return (
     <>
       <div className="App">
+        <RouterProvider router={router} />
       </div>
     </>
   )
