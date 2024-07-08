6f06a29b9c642a5733addd99fc84c7b7f39f606f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-08 21:49:32 +0530

    feat: Create empty expenses page

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import { logoutAction } from "./actions/logout";
 // Routes
 import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
+import { ExpensesPage } from "./pages/ExpensesPage";
 
 const router = createBrowserRouter([
   {
@@ -34,6 +35,10 @@ const router = createBrowserRouter([
       {
         path: "logout",
         action: logoutAction
+      },
+      {
+        path: "expenses",
+        element: <ExpensesPage />,
       }
     ]
   },
