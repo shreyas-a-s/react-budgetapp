2e9eb4b0d07f7385f95222c3208cd2cff626e89a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-08 22:17:58 +0530

    feat: Create 'Budget' column in expense table

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,28 @@
+// rrd imports
+import { Link } from "react-router-dom";
+
 // helper imports
-import { formatCurrency, formatDateToLocaleString } from "../helpers"
+import { formatCurrency, formatDateToLocaleString, getAllMatchingItems } from "../helpers"
 
 const ExpenseItem = ({ expense }) => {
+  const budget = getAllMatchingItems({
+    category: "budgets",
+    key: "id",
+    value: expense.budgetId
+  })[0];
+
   return (
     <>
       <td>{expense.name}</td>
       <td>{formatCurrency(expense.amount)}</td>
       <td>{formatDateToLocaleString(expense.createdAt)}</td>
+      <td>
+        <Link
+        to={`/budget/${budget.id}`}
+        >
+          {budget.name}
+        </Link>
+      </td>
     </>
   )
 }
