25ee462a109d6ed42c7ed58890c02ac5a48d0266
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-08 23:41:50 +0530

    fix: Show 'View all expenses' link only when more than 8 expenses are there
    - This was introduced in commit f3f08f0

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -110,7 +110,7 @@ const { userName, budgets, expenses } = useLoaderData()
                             .slice(0, 8)}
                           />
                           {
-                            expenses.length > 0 && (
+                            expenses.length > 8 && (
                               <Link to="expenses" className="btn btn--dark">
                                 View all expenses
                               </Link>
