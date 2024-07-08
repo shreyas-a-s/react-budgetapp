a69ac5c510c05f1f52cde0aef6868deb66cdebcb
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-08 23:26:56 +0530

    feat: Update deleteItem function in helpers.jsx
    - This is done so that it can also work for deleting expenses

--- src/helpers.jsx
+++ src/helpers.jsx
@@ -17,6 +17,16 @@ export const getAllMatchingItems = ({category, key, value}) => {
   return data.filter((item) => item[key] === value)
 }
 
+// Delete item from local storage
+export const deleteItem = ({ key, id }) => {
+  const existingData = fetchData(key);
+  if (id) {
+    const newData = existingData.filter((item) => item.id !== id);
+    return localStorage.setItem(key, JSON.stringify(newData));
+  }
+  return localStorage.removeItem(key);
+};
+
 // create budget
 export const createBudget = ({ name, amount }) => {
   const newItem = {
@@ -43,11 +53,6 @@ export const createExpense = ({ name, amount, budgetId }) => {
 
   const existingExpenses = fetchData("expenses") ?? []
   return localStorage.setItem("expenses", JSON.stringify([...existingExpenses, newItem]))
-}
-
-// delete item
-export const deleteItem = ({key}) => {
-  return localStorage.removeItem(key)
 }
 
 // total spent by budget
